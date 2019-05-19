dc7de5063ebf5c9d11cc5328a55052cda6024ed8
Hunter <huntercavers@xtra.co.nz>
2019-05-19 13:45:26 +1200

    Initial commit of REsearcher file

---
+++ REsearcher.java
@@ -0,0 +1,14 @@
+// Hunter Cavers (1288108)
+// Sivaram Manoharan (1299026)
+
+class REsearcher
+{
+	public static void main(String[] args)
+	{
+		if(args.length != 1)
+		{
+			System.out.println("Usage: java REsearcher <file name>");
+			return;
+		}
+	}
+}
