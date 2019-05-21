433b8d07127a4fbff4466471a85d7ae8bcde6645
maravis <sivaramigcse@gmail.com>
2019-05-21 14:36:08 +1200

    Added compiler
    Added compiler from tony's notes, still need to adjust a few things.

--- REcompiler.java
+++ REcompiler.java
@@ -2,12 +2,79 @@
 
 public class REcompiler {
 	public static int inputIndex = 0;
+	public static int j = 0;
+	char[] p;
+	int state = 0;
+	char[] ch;
+	int[] next1;
+	int[] next2;
 	public static void main(String[] args) {	
 		String regEx = args[0];
 		regEx += '\0';
 		char[] regString = regEx.toCharArray();
 		System.err.println(regString);
 		parse(regString);
+	}
+	
+	void set_state(int s, char c, int n1, int n2){
+		ch[s]=c;next1[s]=n1;next2[s]=n2;
+	}
+
+	int compileExpression()
+	{
+	  int r;
+
+	  r=compileTerm();
+	  if(isvocab(p[j])||p[j]=='[') {
+		  compileExpression();
+	  }
+	  return(r);
+	}
+
+	int compileTerm()
+	{
+	  int r, t1,t2,f;
+
+	  f=state-1; r=t1=compileFactor();
+	  if(p[j]=='*'){
+	    set_state(state,' ',state+1,t1);
+	    j++; r=state; state++;
+	  }
+	  if(p[j]=='+'){
+	    if(next1[f]==next2[f])
+		next2[f]=state;
+	    next1[f]=state;
+	    f=state-1;
+	    j++;r=state;state++; 
+	    t2=compileTerm();
+	    set_state(r,' ',t1,t2);
+	    if(next1[f]==next2[f])
+		next2[f]=state;
+	    next1[f]=state;
+	  }
+	  return(r);
+	}
+
+	int compileFactor()
+	{
+	  int r= 0;
+	  //int r;
+
+	  if(isvocab(p[j])){
+	    set_state(state,p[j],state+1,state+1);
+	    j++;r=state; state++;
+	  }
+	  else
+	    if(p[j]=='['){
+	      j++; r=compileExpression();
+	      if(p[j]==']')
+		j++;
+	      else
+		error();
+	    }
+	    else
+	      error();
+	  return(r);
 	}
 	
 	static void expression(char[] regString) {
