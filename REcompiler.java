2dfd25a2b55c3d278c82cd3a64ea9d2585b24a81
Sivaram Manoharan <sivaramigcse@gmail.com>
2019-05-21 23:55:00 +1200

    Compiler now outputs the states in an array in the format: Statenum, char,nextstate1,nextstate2
    Fixed some formatting and made compiler work. TODO: find a way to work out how many states there will be ahead of time (currently hardcoded) and code for the other regEx operations.

--- REcompiler.java
+++ REcompiler.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //output is int,char,int,int
 
 public class REcompiler {
@@ -5,23 +7,40 @@ public class REcompiler {
 	public static int j = 0;
 	static char[] p;
 	static int state = 0;
-	static char[] ch;
-	static int[] next1;
-	static int[] next2;
+	static char[] ch = new char[8];
+	static int[] next1 = new int[8];
+	static int[] next2 = new int[8];
 	public static void main(String[] args) {	
 		String regEx = args[0];
 		regEx += '\0';
 		char[] regString = regEx.toCharArray();
+		p = regEx.toCharArray();
 		System.err.println(regString);
 		parse(regString);
+		compileParse();
+//		System.out.println(Arrays.toString(ch));
+//		System.out.println(Arrays.toString(next1));
+//		System.out.println(Arrays.toString(next2));
+		int length = ch.length * 4;
+		String[] outputStates = new String[length];
+		for(int i = 0;i < length/4;i++) {
+			outputStates[i*4] = String.valueOf(i);
+			outputStates[i*4 + 1] = String.valueOf(ch[i]);
+			outputStates[i*4 + 2] = String.valueOf(next1[i]);
+			outputStates[i*4 + 3] = String.valueOf(next2[i]);
+			
+			//outputStates[i] =  String.valueOf(i) + "," + ch[i] + "," + next1[i] + "," + next2[i];
+		}
+		System.out.println(Arrays.toString(outputStates));
 	}
 	
 	static void set_state(int s, char c, int n1, int n2){
-		ch[s]=c;next1[s]=n1;next2[s]=n2;
+		ch[s]=c;
+		next1[s]=n1;
+		next2[s]=n2;
 	}
 
-	static int compileExpression()
-	{
+	static int compileExpression()	{
 	  int r;
 
 	  r=compileTerm();
@@ -31,21 +50,24 @@ public class REcompiler {
 	  return(r);
 	}
 
-	static int compileTerm()
-	{
+	static int compileTerm() {
 	  int r, t1,t2,f;
 
-	  f=state-1; r=t1=compileFactor();
+	  f=state-1;
+	  r=t1=compileFactor();
 	  if(p[j]=='*'){
 	    set_state(state,' ',state+1,t1);
-	    j++; r=state; state++;
+	    j++;
+	    r=state;
+	    state++;
 	  }
 	  if(p[j]=='+'){
 	    if(next1[f]==next2[f])
 		next2[f]=state;
 	    next1[f]=state;
 	    f=state-1;
-	    j++;r=state;state++; 
+	    j++;r=state;
+	    state++; 
 	    t2=compileTerm();
 	    set_state(r,' ',t1,t2);
 	    if(next1[f]==next2[f])
@@ -55,8 +77,7 @@ public class REcompiler {
 	  return(r);
 	}
 
-	static int compileFactor()
-	{
+	static int compileFactor() {
 	  int r= 0;
 	  //int r;
 
@@ -69,11 +90,17 @@ public class REcompiler {
 	      j++; r=compileExpression();
 	      if(p[j]==']')
 		j++;
-	      else
-		error();
+	      else {
+	    	  
+	      		error();
+	      }
+	    	  
 	    }
-	    else
-	      error();
+	    else {
+	    
+		      error();
+	    }
+	    	
 	  return(r);
 	}
 	
@@ -106,31 +133,39 @@ public class REcompiler {
 	      if(regString[inputIndex]==')') {
 	    	  inputIndex++;
 	      }
-	      else error();
+	      else
+	    	  
+	    	  error();
 	    }
-	    else error();
+	    else {
+	    	error();
+	    }
+	    	
 	}
 
 	static void parse(char[] regString) {
 		
 	   expression(regString);
 	   if(regString[inputIndex] != '\0' ) {
+		  
 		   error();
 	   }
-	   System.err.println("REACHED END");
+	   
 	}
 	
-	static void compileParse()
-	{
+	static void compileParse() {
 	  int initial;
 
 	  initial= compileExpression();
-	  if( p[j] == 0) error(); // In C, zero is false, not zero is true
+	  if( p[j] != '\0') {
+		  
+		  error(); // In C, zero is false, not zero is true
+	  }
 	  set_state(state,' ',0,0);
 	}
 	
 	static void error() {
-		System.err.println("you messed up boi");
+		System.err.println("Error: Input is not a wellformed regexp");
 	}
 	
 	static boolean isvocab(char currChar) {
